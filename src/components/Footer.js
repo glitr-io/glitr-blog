8dfe056adbb8449410f16c27be880c6908f79baa
xoron <rmaher84@gmail.com>
2019-05-28 23:28:38 +0200

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,23 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
+
+        <div id="disqus_thread"></div>
+        <script>
+          var disqus_config = function () {
+            this.page.url = window.location.href;
+            this.page.identifier = window.location.href;
+          };
+          (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://glitr-io.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+          })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <script id="dsq-count-scr" src="//glitr-io.disqus.com/count.js" async></script>
+
         <div className="content has-text-centered">
           <img
             src={logo}
