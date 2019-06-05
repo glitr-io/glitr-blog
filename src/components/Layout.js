7b322d7e303c37c0110e5cb2655b39168ad182b8
xoron <rmaher84@gmail.com>
2019-06-06 01:12:04 +0200

    google analytics attempt 1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,6 +43,15 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
         <meta property="og:image" content="/img/og-image.jpg" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71764908-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-71764908-1');
+        </script>
       </Helmet>
       <Navbar />
       <div>{children}</div>
